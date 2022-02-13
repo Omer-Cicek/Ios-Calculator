4d658b5b0790070a5e8804abc90bfc64f44f7182
Omer-Cicek <omercicek160408@gmail.com>
2022-02-13 18:08:02 +0300

    js mathmatical operations done

--- App.js
+++ App.js
@@ -5,22 +5,73 @@ const btn2 = document.querySelector('.button2');
 const btn3 = document.querySelector('.button3');
 const operands = document.querySelectorAll('.operand');
 
-const num1 = [];
-const num2 = [];
-const arr = [];
+let arr = [];
+let arr1 = [];
+let arr2 = [];
+//kontrol fonksiyonu
+const controlFunction = () => {
+  if (arr[0] == '+') {
+    let ömer = +arr1.join('') + +arr2.join('');
+    arr1 = [ömer];
+    arr2 = [];
+    console.log(arr1);
+    console.log(arr2);
+  }
+  if (arr[0] == '-') {
+    let ömer = +arr1.join('') - +arr2.join('');
+    arr1 = [ömer];
+    arr2 = [];
+    console.log(arr1);
+    console.log(arr2);
+  }
+  if (arr[0] == '*') {
+    let ömer = +arr1.join('') * (+arr2.join('') == 0 ? 1 : +arr2.join(''));
+    console.log(ömer);
+    arr1 = [ömer];
+    arr2 = [];
+    console.log(arr1);
+    console.log(arr2);
+  }
+  if (arr[0] == '/') {
+    let ömer = +arr1.join('') / (+arr2.join('') == 0 ? 1 : +arr2.join(''));
+    arr1 = [ömer];
+    arr2 = [];
+    console.log(arr1);
+    console.log(arr2);
+  }
+};
+//AddEventListener Function
 innerContainer.addEventListener('click', (e) => {
-  //First Number Part
-
-  if (e.target.classList.contains('num')) {
-    num1.push(e.target.value);
+  //tıklanan nesnenin classında operand varsa
+  if (e.target.classList.contains('operand')) {
+    arr.push(e.target.value);
+  }
+  //arr dosyasında operand olmadığında arr1 e pushla
+  if (arr.length == 0) {
+    arr1.push(e.target.value);
+  }
+  //arr dosyasında operand olduğunda arr2 e pushla
+  if (arr.length == 1 && e.target.classList.contains('num')) {
+    arr2.push(e.target.value);
   }
 
-  //Selecting operant
-  operands.forEach((ope) => {
-    if (e.target.value == ope.value && e.target.classList.contains('operand')) {
-      num1.pop();
-      num1.push(e.target.value);
-    }
-  });
-  console.log(num1.join('').split('+'));
+  //eğer ki arr de 1 den fazla operand varsa
+  if (arr.length >= 2) {
+    controlFunction();
+    arr.shift();
+    console.log(arr);
+  }
+  //eşittire basınca
+  if (e.target.value == '=') {
+    controlFunction();
+  }
+  // ac basınca
+  if (e.target.value == 'ac') {
+    arr = [];
+    arr1 = [];
+    arr2 = [];
+  }
+  console.log(arr);
+  console.log(arr1);
+  console.log(arr2);
 });
